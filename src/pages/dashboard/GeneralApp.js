7fda7d701da9d1bfc5d5aca219d84b05dd8791e5
RushabhRamani123 <129403338+RushabhRamani123@users.noreply.github.com>
2024-01-06 17:50:59 +0530

    styl-contact_info

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -3,16 +3,20 @@ import React from "react";
 import Conversation from "../../components/Conversation";
 import Chats from "./Chats";
 import { useTheme } from "@mui/material/styles";
-
+import Contact from "../../components/Contact";
+import { useSelector } from "react-redux";
 const GeneralApp = () => {
   const theme = useTheme();
+  const { sideBar } = useSelector((state) => state.app);
   return (
     <Stack direction={"row"} sx={{ width: "100%" }}>
       <Chats />
       <Box
         sx={{
           height: "100%",
-          width: "calc(100vw - 420px)",
+          width: sideBar.open
+              ? `calc(100vw - 740px )`
+              : "calc(100vw - 420px )",
           backgroundColor:
             theme.palette.mode === "light"
               ? "#fff"
@@ -21,6 +25,8 @@ const GeneralApp = () => {
       >
       <Conversation />
       </Box>
+      {/* contacts */}
+      {sideBar.open && <Contact/>}
     </Stack>
   );
 };
