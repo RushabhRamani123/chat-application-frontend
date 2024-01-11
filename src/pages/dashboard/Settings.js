59c1a1120157474d405a202c1056ce869f444f08
RushabhRamani123 <129403338+RushabhRamani123@users.noreply.github.com>
2024-01-11 11:31:24 +0530

    style-settings page

--- src/pages/dashboard/Settings.js
+++ src/pages/dashboard/Settings.js
@@ -7,8 +7,17 @@ const Settings = () => {
       <>
           <Stack direction={"row"} sx={{ width: '100%' }}>
               {/* LeftPannel */}
-              <Box sx={{ overFlowY: 'scroll', height: '100vh', width: 320, backgroundColor: theme.palette.mode === 'light' ? '#F8FAFF' : theme.palette.background }}>
-                  
+              <Box sx={{
+                  overFlowY: 'scroll',
+                  height: '100vh', width: 320,
+                  backgroundColor: theme.palette.mode === 'light' ? '#F8FAFF' : theme.palette.background,
+                  boxShadow: '0px 0px 2px rgba(0, 0, 0, 0.25)',
+              }}>
+                  <Stack p={4} spacing={5}>
+                      {/* Header */}
+                      {/* Profile */}
+                      {/* List of Option */}
+                  </Stack>
               </Box>
               {/* RightPannel */}
       </Stack>
