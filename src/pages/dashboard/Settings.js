c4e89282e83b9b45bdfe598b4ac594d53ce0c3b6
RushabhRamani123 <129403338+RushabhRamani123@users.noreply.github.com>
2024-01-12 21:02:55 +0530

    style-settings page

--- src/pages/dashboard/Settings.js
+++ src/pages/dashboard/Settings.js
@@ -1,28 +1,190 @@
-import React from 'react'
-import { Stack, Box } from '@mui/material'
-import { useTheme } from '@mui/material/styles';
-const Settings = () => {
-    const theme = useTheme()
-  return (
-      <>
-          <Stack direction={"row"} sx={{ width: '100%' }}>
-              {/* LeftPannel */}
-              <Box sx={{
-                  overFlowY: 'scroll',
-                  height: '100vh', width: 320,
-                  backgroundColor: theme.palette.mode === 'light' ? '#F8FAFF' : theme.palette.background,
-                  boxShadow: '0px 0px 2px rgba(0, 0, 0, 0.25)',
-              }}>
-                  <Stack p={4} spacing={5}>
-                      {/* Header */}
-                      {/* Profile */}
-                      {/* List of Option */}
-                  </Stack>
-              </Box>
-              {/* RightPannel */}
-      </Stack>
-      </>
-  )
-}
+import React, { useState } from "react";
+import {
+  Avatar,
+  Box,
+  Divider,
+  IconButton,
+  Stack,
+  Typography,
+} from "@mui/material";
 
-export default Settings
+import {
+  CaretLeft,
+  Bell,
+  Lock,
+  Key,
+  PencilCircle,
+  Image,
+  Note,
+  Keyboard,
+  Info,
+} from "phosphor-react";
+
+import { useTheme } from "@mui/material/styles";
+import { faker } from "@faker-js/faker";
+import SimpleBar from 'simplebar-react';
+import 'simplebar-react/dist/simplebar.min.css';
+// import ThemeDialog from "../../sections/dashboard/Settings/ThemeDialog";
+// import ShortcutDialog from "../../sections/dashboard/Settings/ShortcutDialog";
+
+const Settings = () => {
+  const theme = useTheme();
+
+  const [openTheme, setOpenTheme] = useState(false);
+
+  const handleOpenTheme = () => {
+    setOpenTheme(true);
+  };
+
+  const handleCloseTheme = () => {
+    setOpenTheme(false);
+  };
+  const [openShortcuts, setOpenShortcuts] = useState(false);
+
+  const handleOpenShortcuts = () => {
+    setOpenShortcuts(true);
+  };
+
+  const handleCloseShortcuts = () => {
+    setOpenShortcuts(false);
+  };
+
+  const list = [
+    {
+      key: 0,
+      icon: <Bell size={20} />,
+      title: "Notifications",
+      onclick: () => {},
+    },
+    {
+      key: 1,
+      icon: <Lock size={20} />,
+      title: "Privacy",
+      onclick: () => {},
+    },
+    {
+      key: 2,
+      icon: <Key size={20} />,
+      title: "Security",
+      onclick: () => {},
+    },
+    {
+      key: 3,
+      icon: <PencilCircle size={20} />,
+      title: "Theme",
+      onclick: handleOpenTheme,
+    },
+    {
+      key: 4,
+      icon: <Image size={20} />,
+      title: "Chat Wallpaper",
+      onclick: () => {},
+    },
+    {
+      key: 5,
+      icon: <Note size={20} />,
+      title: "Request Account Info",
+      onclick: () => {},
+    },
+    {
+      key: 6,
+      icon: <Keyboard size={20} />,
+      title: "Keyboard Shortcuts",
+      onclick: handleOpenShortcuts,
+    },
+    {
+      key: 7,
+      icon: <Info size={20} />,
+      title: "Help",
+      onclick: () => {},
+    },
+  ];
+
+  return (
+    <>
+      <Stack direction="row" sx={{ width: "100%" }}>
+        {/* LeftPane */}
+        <Box
+          sx={{
+            overflowY: "scroll",
+            
+            height: "100vh",
+            width: 320,
+            backgroundColor:
+              theme.palette.mode === "light"
+                ? "#F8FAFF"
+                : theme.palette.background,
+
+                      boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
+                      "&::-webkit-scrollbar": { display: "none" },
+                    }}
+        >
+                    <SimpleBar style={{ height: "100vh" }} timeout={500} clickOnTrack={false}>
+          
+          <Stack p={4} spacing={5}>
+            {/* Header */}
+            <Stack direction="row" alignItems={"center"} spacing={3}>
+              <IconButton>
+                <CaretLeft size={24} color={"#4B4B4B"} />
+              </IconButton>
+
+              <Typography variant="h6">Settings</Typography>
+            </Stack>
+
+            {/* Profile */}
+            <Stack direction="row" spacing={3}>
+              <Avatar
+                src={faker.image.avatar()}
+                sx={{ height: 56, width: 56 }}
+              />
+              <Stack spacing={0.5}>
+                <Typography variant="article">{`${faker.name.firstName()} ${faker.name.lastName()}`}</Typography>
+                <Typography variant="body2">{faker.random.words()}</Typography>
+              </Stack>
+            </Stack>
+            {/* List */}
+            <Stack spacing={4} sx={{ }}>
+              {list.map(({ key, icon, title, onclick }) => {
+                return (
+                  <>
+                    <Stack
+                      onClick={onclick}
+                      sx={{ cursor: "pointer" }}
+                      spacing={2}
+                    >
+                      <Stack alignItems={"center"} direction="row" spacing={2}>
+                        {icon}
+                        <Typography variant="body2">{title}</Typography>
+                      </Stack>
+                      {key !== 7 && <Divider />}
+                    </Stack>
+                  </>
+                );
+              })}
+            </Stack>
+          </Stack>
+        </SimpleBar>
+          </Box>
+        {/* Right Pane */}
+        {/* <Box
+          sx={{
+            height: "100%",
+            width: "calc(100vw - 420px )",
+            backgroundColor:
+              theme.palette.mode === "light"
+                ? "#FFF"
+                : theme.palette.background.paper,
+            borderBottom: "6px solid #0162C4",
+          }}
+        ></Box> */}
+      </Stack>
+      {/* {openTheme && (
+        <ThemeDialog open={openTheme} handleClose={handleCloseTheme} />
+      )}
+      {openShortcuts && <ShortcutDialog open={openShortcuts} handleClose={handleCloseShortcuts} /> } */}
+      
+    </>
+  );
+};
+
+export default Settings;
